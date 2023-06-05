396bc3920b32dfae76f221af54f9aae2f030400e
Erdim <erdim.kinik@vrpconsulting.com>
2023-06-05 20:54:53 +0200

    router bug was fixed

--- routes/blog/post.js
+++ routes/blog/post.js
@@ -1,9 +1,9 @@
 const router = require('express').Router();
 const PostController = require('../../controllers/PostController');
 
-router.get('/', PostController.getPostsController);
-router.post('/:postID', PostController.likePostController);
 router.post('/create', PostController.createPostController);
 router.get('/myposts', PostController.getMyPostsController);
+router.post('/:postID', PostController.likePostController);
+router.get('/', PostController.getPostsController);
 
 module.exports = router;
